dc6d631072d0de96f4c5ddd2beaba3321f1b85d2
John Rork <jnrork@gmail.com>
2014-01-21 22:42:50 -0600

    forgot these changes in the last push

--- __init__.py
+++ __init__.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.debug = True
-app.user = 'johnrork'
+app.user = {'username':'test', 'password': 'password'}
 app.secret_key = 'sekrit'
+app.results_per = 10
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Chinook_Sqlite_AutoIncrementPKs.sqlite'
+db = SQLAlchemy(app)
+
+
+class Album(db.Model):
+	__table__ = db.Table('Album', db.metadata, autoload=True, autoload_with=db.engine)
 
 
 def valid_login():
@@ -18,28 +26,38 @@ def valid_login():
 
 @app.before_request
 def check():
-	if not ('login' in request.path or 'logout' in request.path) and not valid_login():
+	if not 'login' or 'logout' in request.path and not valid_login():
 		return redirect(url_for('login'))
-
-
-@app.route('/macros')
-def list():
-	l =[{'foo':'bar', 'bar': 'baz'}, {'foo':'bar', 'bar': 'baz'}]
-	cols = ['foo', 'bar']
-	return render_template('macros.html', list=l, c=cols)
 
 
 @app.route('/')
 def home():
-	return 'hi'
+	return redirect(url_for('list'))
+
+
+@app.route('/albums/')
+@app.route('/albums/<int:page>/')
+def list(page=1):
+	data = Album.query
+
+	if request.args.get('sort'):
+		order = request.args.get('sort')
+		if request.args.get('dir') == 'desc':
+			order = db.desc(order)
+		data = data.order_by(order)
+	return render_template('list.html', 
+							list=data.paginate(page), 
+							cols=['Title'], 
+							params=params)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	if request.method == 'POST':
-		if request.form['username'] == app.user:
+		if (request.form['username'] == app.user['username'] 
+			and request.form['password'] == app.user['password']):
 			session['logged_in'] = datetime.datetime.now()	
-			return 'success'
+			return redirect(url_for('home'))
 		else:
 			return 'fail'
 	return render_template('login.html')
@@ -49,7 +67,7 @@ def login():
 def logout():
 	if 'logged_in' in session:
 		session.pop('logged_in')
-	return 'logout'
+	return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
